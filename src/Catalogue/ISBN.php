0c42e3557cb4c78f78b61dbab4cb422f7b42808f
Mateusz Cholewka <mateusz@cholewka.com.pl>
2020-11-07 13:29:09 +0100

    Change alias name, and remove validator from state

--- src/Catalogue/ISBN.php
+++ src/Catalogue/ISBN.php
@@ -5,19 +5,15 @@ declare(strict_types=1);
 namespace Akondas\Library\Catalogue;
 
 use InvalidArgumentException;
-use Isbn\Isbn as FaleIsbn;
+use Isbn\Isbn as IsbnCode;
 
 class ISBN
 {
-    private FaleIsbn $faleIsbn;
-
     private string $isbn;
 
     public function __construct(string $isbn)
     {
-        $this->faleIsbn = new FaleIsbn();
-
-        if (!$this->faleIsbn->validation->isbn($isbn)) {
+        if (!(new IsbnCode())->validation->isbn($isbn)) {
             throw new InvalidArgumentException('Invalid ISBN');
         }
 
