a029678c94f232634edf290beda12383a890bf99
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2024-06-13 08:21:22 +0200

    install symfony and doctrine (#18)
    * add symfony and doctrine
    
    * fix ci db url

---
+++ config/bundles.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+];
---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Akondas\Library\Common\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
